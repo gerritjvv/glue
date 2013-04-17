710b3ce64ff17fae7111cedc205b9c0f79e4c6a7
gerritjvv <gerritjvv@gmail.com>
2013-04-17 16:55:50 +0200

    Fixes #19

--- core/glue-unit/src/main/groovy/org/glue/unit/om/impl/jython/Closure.groovy
+++ core/glue-unit/src/main/groovy/org/glue/unit/om/impl/jython/Closure.groovy
@@ -10,22 +10,39 @@ import org.python.core.PyObject
 class Closure<V> extends groovy.lang.Closure<V>{
 
 	PyObject pyObj;
+	PyObject[] ctxArgs = null;
 	
 	Closure(PyObject v){
+		this(v, null)
+	}
+	
+	
+	Closure(PyObject v, PyObject... ctxArgs){
 		super(v)
 		
 		if(v == null)
 		  throw new IllegalArgumentException("Parameter for Closure cannot be null")
 		  
+		
 		this.pyObj = v
+		if(ctxArgs != null && ctxArgs.length > 0 && ctxArgs[0] != null)
+			this.ctxArgs = ctxArgs
 	}
 	
 	def V call(Object... args){
-		PyObject[] pyObjs = new PyObject[args.length];
 		
-		for(int i = 0; i < args.length; i++){
-			pyObjs[i] = PythonContextAdaptor.derive(args[i])
-		}	
+		int ctxLen = (ctxArgs == null) ? 0 : ctxArgs.length
+		
+		PyObject[] pyObjs = new PyObject[ctxLen + args.length];
+		
+		int i  = 0;
+		
+		for(; i < ctxLen; i++)
+		   pyObjs[i] = ctxArgs[i]
+		
+		
+		for(; i < args.length + ctxLen; i++)
+			pyObjs[i] = PythonContextAdaptor.derive(args[ i-ctxLen ])
 			
 		pyObj.__call__(pyObjs)
 	}
